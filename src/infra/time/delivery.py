cb061baaf098083bef9cc57fe01f49b96a882c2a
Ashu <ashufrancis673@gmail.com>
2026-09-26 14:20:37 +0200

    update time

--- src/infra/time/delivery.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime, timezone
-
-
-class SystemDeliveryClock:
-    def now(self) -> datetime:
-        return datetime.now(timezone.utc)
-
-
-__all__ = ["SystemDeliveryClock"]
